b8ade5a7ab089c820a20355193ae3f753df1d0b8
nilsantana <nilsantana@yahoo.com.br>
2018-10-16 03:03:12 -0300

    estudo de POO em PHP

---
+++ Livro.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Livro
+ *
+ * @author Nil
+ */
+class Livro {
+    //put your code here
+}
